ec056836f7d84c3a6f52aa73926e1fed6e0a42cc
wblucca <wblucca@wpi.edu>
2019-11-03 16:33:08 -0500

    Fix toString in Chunk class

--- src/levelGenerators/JariwalaLuccaGenerator/LevelGenerator.java
+++ src/levelGenerators/JariwalaLuccaGenerator/LevelGenerator.java
@@ -565,8 +565,8 @@ class Chunk {
     public String toString() {
         String blocksAsStr = "";
 
-        for (int x = 0; x < getWidth(); x++) {
-            for (int y = 0; y < getHeight(); y++) {
+        for (int y = 0; y < getHeight(); y++) {
+            for (int x = 0; x < getWidth(); x++) {
                 blocksAsStr += blocks[x][y];
             }
             blocksAsStr += "\n";
