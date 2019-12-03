48120e221c0ea70cd2649c51fa5224cc40061f16
Neer Jariwala <njariwala@gmail.com>
2019-12-02 19:40:14 -0500

    mario can walk... and now jump

--- src/agents/JariwalaLuccaAgent/Walk.java
+++ src/agents/JariwalaLuccaAgent/Walk.java
@@ -13,9 +13,18 @@ public class Walk extends Task{
 
     @Override
     public boolean run(Agent agent) {
-        //MarioForwardModel model = null;
-        //int[][] info = model.getMarioEnemiesObservation(0);
-        agent.setAction(Helper.createAction(false, true, false, false, false));
+        float[] marioPos = model.getMarioFloatPos();
+        float[] enemyPos = model.getEnemiesFloatPos();
+        for (int i = 0; i < enemyPos.length-2; i++) {
+            float enemyx = enemyPos[3 * i + 1];
+            float enemyy = enemyPos[3 * i + 2];
+            if (Math.abs(enemyx) - marioPos[0] < 100) {
+                agent.setAction(Helper.createAction(false, true, false, true, false));
+            }
+            else {
+                agent.setAction(Helper.createAction(false, true, false, false, false));
+            }
+        }
         return true;
     }
 }
