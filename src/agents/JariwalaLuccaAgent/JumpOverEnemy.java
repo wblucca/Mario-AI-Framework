1ece08aea5161d0f5fd6be3ccc6cbeeec1bd097d
Neer Jariwala <njariwala@gmail.com>
2019-12-04 14:14:00 -0500

    better pipe detection and enemy detection

--- src/agents/JariwalaLuccaAgent/JumpOverEnemy.java
+++ src/agents/JariwalaLuccaAgent/JumpOverEnemy.java
@@ -14,8 +14,15 @@ public class JumpOverEnemy extends Task{
             float enemyx = enemyPos[3 * i + 1];
             float enemyy = enemyPos[3 * i + 2];
             if (Math.abs(enemyx) - marioPos[0] < 150 && Math.abs(marioPos[1] - enemyy) < 48) {
-                agent.setAction(Helper.createAction(false, true, false, true, false));
-                return true;
+                if (Math.abs(enemyx) - marioPos[0] < 6 && Math.abs(marioPos[1] - enemyy) < 6) {
+                    agent.setAction(Helper.createAction(true, false, false, true, false));
+                    return true;
+                }
+                else {
+                    agent.setAction(Helper.createAction(false, true, false, true, false));
+                    return true;
+                }
+
 
             }
             else {
--- src/agents/JariwalaLuccaAgent/JumpOverPipe.java
+++ src/agents/JariwalaLuccaAgent/JumpOverPipe.java
@@ -11,7 +11,7 @@ public class JumpOverPipe extends Task {
         int[][] scene = model.getMarioSceneObservation();
         int[][] scene2 = model.getMarioSceneObservation(0);
             for (int j = 6; j < scene2[15].length; j++) {
-                if (scene2[16][j] == 34 || scene2[15][j] == 18) {
+                if (scene2[15][j] == 34 || scene2[16][j] == 34 || scene2[15][j] == 18) {
                     agent.setAction(Helper.createAction(false, true, false, true, false));
                     return true;
                 }
