12fc84aad422376dd0fb1bbd14ed775509204997
wblucca <wblucca@wpi.edu>
2019-12-04 19:42:33 -0500

    Enemy detection a little more accurate

--- src/agents/JariwalaLuccaAgent/JumpOverEnemy.java
+++ src/agents/JariwalaLuccaAgent/JumpOverEnemy.java
@@ -13,7 +13,7 @@ public class JumpOverEnemy extends Task{
         for (int i = 0; i < enemyPos.length/3; i++) {
             float enemyx = enemyPos[3 * i + 1];
             float enemyy = enemyPos[3 * i + 2];
-            if (Math.abs(enemyx - marioPos[0]) < 150 && Math.abs(marioPos[1] - enemyy) < 48) {
+            if (Math.abs(enemyx - marioPos[0]) < 64 && marioPos[1] - enemyy < 10) {
                 if (Math.abs(enemyx - marioPos[0]) < 6 && Math.abs(marioPos[1] - enemyy) < 6) {
                     agent.setAction(Helper.createAction(true, false, false, true, false));
                     return true;
@@ -22,7 +22,6 @@ public class JumpOverEnemy extends Task{
                     agent.setAction(Helper.createAction(false, true, false, true, false));
                     return true;
                 }
-
 
             }
         }
